b95f4e06abc4d89ef6a8fb6ef6b529908a5c1663
Merel Jacobs <merel.burleigh@gmail.com>
2023-12-13 18:23:12 -0800

    inital push

---
+++ backend/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'igh4re44',
+    dataset: 'production'
+  }
+})
---
+++ backend/schemas/testimonials.js
@@ -0,0 +1,12 @@
+export default {
+    name: 'testimonials',
+    title: "Testimonials",
+    type: "document",
+    fields: [
+        {
+            name: 'name',
+            title: 'title',
+            type: 'string'
+        }
+    ]
+}
